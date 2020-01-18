b8c85698b7b6cb370cd3c3b96bb462e9b6942c18
kss27 <arkan23a@yandex.ru>
2020-01-18 18:03:07 +0300

    Init commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage();
+  @override
+  Widget build(BuildContext context) {
+    return Text('Temp text');
+  }
+}
